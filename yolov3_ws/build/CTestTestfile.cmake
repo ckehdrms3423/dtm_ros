5c8ce765b7f7a6dc0398366e548871c101ce5314
ckehdrms3423 <ckehdrms3423@gmail.com>
2022-10-25 13:05:09 +0900

    second

--- yolov3_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/idp/dtm_ros/yolov3_ws/src
-# Build directory: /home/idp/dtm_ros/yolov3_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("darknet_ros/darknet_ros_msgs")
-subdirs("darknet_ros/darknet_ros")
--- yolov3_ws/build/darknet_ros/darknet_ros_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/idp/dtm_ros/yolov3_ws/src/darknet_ros/darknet_ros_msgs
-# Build directory: /home/idp/dtm_ros/yolov3_ws/build/darknet_ros/darknet_ros_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
