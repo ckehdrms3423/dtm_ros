b40b0e159c3669e65e1b98a40bae86b72f5e9ed7
ckehdrms3423 <ckehdrms3423@gmail.com>
2022-10-25 12:49:47 +0900

    re

---
+++ yolov3_ws/build/darknet_ros/darknet_ros_msgs/CMakeFiles/darknet_ros_msgs_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/darknet_ros_msgs_generate_messages_py"
+  "/home/idp/dtm_ros/yolov3_ws/devel/lib/python2.7/dist-packages/darknet_ros_msgs/msg/_CheckForObjectsActionGoal.py"
+  "/home/idp/dtm_ros/yolov3_ws/devel/lib/python2.7/dist-packages/darknet_ros_msgs/msg/_CheckForObjectsActionResult.py"
+  "/home/idp/dtm_ros/yolov3_ws/devel/lib/python2.7/dist-packages/darknet_ros_msgs/msg/_BoundingBox.py"
+  "/home/idp/dtm_ros/yolov3_ws/devel/lib/python2.7/dist-packages/darknet_ros_msgs/msg/_CheckForObjectsGoal.py"
+  "/home/idp/dtm_ros/yolov3_ws/devel/lib/python2.7/dist-packages/darknet_ros_msgs/msg/_BoundingBoxes.py"
+  "/home/idp/dtm_ros/yolov3_ws/devel/lib/python2.7/dist-packages/darknet_ros_msgs/msg/_CheckForObjectsResult.py"
+  "/home/idp/dtm_ros/yolov3_ws/devel/lib/python2.7/dist-packages/darknet_ros_msgs/msg/_CheckForObjectsFeedback.py"
+  "/home/idp/dtm_ros/yolov3_ws/devel/lib/python2.7/dist-packages/darknet_ros_msgs/msg/_CheckForObjectsActionFeedback.py"
+  "/home/idp/dtm_ros/yolov3_ws/devel/lib/python2.7/dist-packages/darknet_ros_msgs/msg/_CheckForObjectsAction.py"
+  "/home/idp/dtm_ros/yolov3_ws/devel/lib/python2.7/dist-packages/darknet_ros_msgs/msg/_ObjectCount.py"
+  "/home/idp/dtm_ros/yolov3_ws/devel/lib/python2.7/dist-packages/darknet_ros_msgs/msg/__init__.py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/darknet_ros_msgs_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
